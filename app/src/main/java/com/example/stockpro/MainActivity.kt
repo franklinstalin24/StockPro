d73e3350c5568ddb8eac41774ba1ca30591a94fe
franklin_cuascota <franklin.cuascota@ister.edu.ec>
2026-06-14 16:43:43 -0500

    configuracion de la estructura de navegacion

--- app/src/main/java/com/example/stockpro/MainActivity.kt
+++ app/src/main/java/com/example/stockpro/MainActivity.kt
@@ -11,6 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.stockpro.ui.theme.StockProTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +23,36 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             StockProTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                // Función que contiene toda la lógica de navegación y pantallas
+                StockProApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun StockProApp(viewModel: StockViewModel = viewModel()) {
+    val navController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StockProTheme {
-        Greeting("Android")
+    // El navhost controla el paso entre las 4 pantallas
+    NavHost(navController = navController, startDestination = "pantalla1") {
+
+        composable("pantalla1") {
+            PantallaIngreso(navController) // Pantalla de ingreso con el TextField para el nombre del operario
+        }
+
+        composable("pantalla2") { backStackEntry ->
+            val nombre = backStackEntry.arguments?.getString("nombre") ?: ""
+            PantallaCatalogo(navController, nombre, viewModel) // Pantalla de catálogo que muestra el nombre del operario y la lista de productos
+        }
+
+        composable("pantalla3") { backStackEntry ->
+            val id = backStackEntry.arguments?.getString("id")?.toIntOrNull() ?: 0
+            PantallaEdicion(navController, id, viewModel) // Pantalla de edición que muestra el detalle del producto seleccionado y permite actualizar el stock
+        }
+
+        composable("pantalla4") {
+            PantallaReporte(navController, viewModel) // Pantalla de reporte que muestra un resumen del stock actual, productos críticos y productos sin stock
+        }
     }
 }
